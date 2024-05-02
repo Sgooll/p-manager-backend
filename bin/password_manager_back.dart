5115880cd561a71555180180b8417730d978cae6
Sgooll <vitalifting2@mail.ru>
2024-05-02 22:53:02 +0300

    add cert

--- bin/password_manager_back.dart
+++ bin/password_manager_back.dart
@@ -12,9 +12,16 @@ void main() async {
 
   Api server = Api(connection);
 
+  final securityContext = SecurityContext(
+  );
+
+  securityContext.setTrustedCertificates('public.crt');
+
   var servedServer = await shelf_io.serve(
       server.handler, '178.208.85.222', 8080,
-      securityContext: SecurityContext());
+      securityContext: securityContext,
+   );
+
 
   print('Serving at https://${servedServer.address.host}:${servedServer.port}');
   final server1 = await ServerSocket.bind('178.208.85.222', 3000);
