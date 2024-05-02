02b4a8effc0bfaff07dcbfe7c304860f1577c6fe
Sgooll <vitalifting2@mail.ru>
2024-05-02 22:39:13 +0300

    security context

--- bin/password_manager_back.dart
+++ bin/password_manager_back.dart
@@ -12,13 +12,16 @@ void main() async {
 
   Api server = Api(connection);
 
-  var servedServer = await shelf_io.serve(server.handler, '178.208.85.222', 8080);
+  var servedServer = await shelf_io.serve(
+      server.handler, '178.208.85.222', 8080,
+      securityContext: SecurityContext());
 
-  print('Serving at http://${servedServer.address.host}:${servedServer.port}');
+  print('Serving at https://${servedServer.address.host}:${servedServer.port}');
   final server1 = await ServerSocket.bind('178.208.85.222', 3000);
   print("WSServer is running on: 178.208.85.222:3000");
   server1.listen((Socket client) {
     clients.add(client);
-    print('getConnection - ${client.remoteAddress.address}:${client.remotePort}');
+    print(
+        'getConnection - ${client.remoteAddress.address}:${client.remotePort}');
   });
 }
