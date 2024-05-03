28a3dbdc3737069c79a6d8e1d1b95dda473fe9aa
Sgooll <vitalifting2@mail.ru>
2024-05-03 11:45:05 +0300

    domen and ssl

--- bin/password_manager_back.dart
+++ bin/password_manager_back.dart
@@ -12,20 +12,21 @@ void main() async {
 
   Api server = Api(connection);
 
-  final securityContext = SecurityContext(
-  );
+  final securityContext = SecurityContext();
 
-  securityContext.setTrustedCertificates('public.crt');
+  securityContext.useCertificateChain(
+      "/etc/letsencrypt/live/pmanager-api.ru/fullchain.pem");
 
   var servedServer = await shelf_io.serve(
-      server.handler, '178.208.85.222', 8080,
-      securityContext: securityContext,
-   );
-
+    server.handler,
+    'pmanager-api.ru',
+    8080,
+    securityContext: securityContext,
+  );
 
   print('Serving at https://${servedServer.address.host}:${servedServer.port}');
-  final server1 = await ServerSocket.bind('178.208.85.222', 3000);
-  print("WSServer is running on: 178.208.85.222:3000");
+  final server1 = await ServerSocket.bind('pmanager-api.ru', 3000);
+  print("WSServer is running on: pmanager-api.ru:3000");
   server1.listen((Socket client) {
     clients.add(client);
     print(
